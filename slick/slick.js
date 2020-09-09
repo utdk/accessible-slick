68081a6bc2567a3e85c693b598a0407c301e21cc
Jason Webb <zen.webb@gmail.com>
2020-09-09 15:01:46 -0500

    #3 updating metadata and links inside slick.js

--- slick/slick.js
+++ slick/slick.js
@@ -1,4 +1,5 @@
 /*
+       (accessible)
      _ _      _       _
  ___| (_) ___| | __  (_)___
 / __| | |/ __| |/ /  | / __|
@@ -6,12 +7,12 @@
 |___/_|_|\___|_|\_(_)/ |___/
                    |__/
 
- Version: 1.8.1
-  Author: Ken Wheeler
- Website: http://kenwheeler.github.io
-    Docs: http://kenwheeler.github.io/slick
-    Repo: http://github.com/kenwheeler/slick
-  Issues: http://github.com/kenwheeler/slick/issues
+ Version: 1.8.1@accessible360.1
+  Author: Jason Webb (Accessible360)
+ Website: https://accessible360.com
+    Docs: https://accessible360.github.io/accessible-slick
+    Repo: https://github.com/Accessible360/accessible-slick
+  Issues: https://github.com/Accessible360/accessible-slick/issues
 
  */
 /* global window, document, define, jQuery, setInterval, clearInterval */
